f8e7fcb324fa6cc285b4728161fcc3d050e5a4f6
Jim <jim.lemmers@gmail.com>
2015-10-06 17:02:18 +0200

    started on commandline arguments and added some prints with information

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import slumber
 import requests
 import os
+import sys
+import getopt
 
-show_objs = True
+
 api = slumber.API(base_url='http://readthedocs.org/api/v1/')
 
 
@@ -12,28 +14,40 @@ def download_epub(libraries):
         if '==' in library:
             name, version = library.split('==')
         else:
-            name, version = library.lower().strip(), 'latest'
+            name, version = library.lower().strip(), ''
 
-        project = api.project.get(slug=name)
-        print(project)
+        project = api.project(name).get()
         if project['objects']:
-            epub_url = project['objects'][0]['downloads']['epub']
-            epub = requests.get('http:' + epub_url).content
-            with open('%s-%s.epub' % (name, version), 'wb') as out:
-                out.write(epub)
+            downloads = project['objects'][0]['downloads']
+            if downloads.get('epub'):
+                print('Downloading %s docs for %s' % (version, name))
+                doc = requests.get('http:' + downloads.get('epub')).content
+                with open('%s-%s.epub' % (name, version), 'wb') as out:
+                    out.write(doc)
+                print('Docs saved!')
+            else:
+                print('Downloading %s docs for %s' % (version, name))
+                doc = requests.get('http://' + downloads.get('htmlzip'))
 
 
-if 'requirements.txt' in os.listdir():
-    with open('requirements.txt') as installed:
-        libraries = installed.readlines()
-        download_epub(libraries)
-else:
-    print('Please enter a list of libraries seperated by a comma')
-    print('If you need to specify a version, please do so by:')
-    print('"name==version"')
-    libraries = str(input(':')).split(',')
-    if libraries:
-        download_epub(libraries)
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "h")
+    except:
+        pass
+    if 'requirements.txt' in os.listdir():
+        with open('requirements.txt') as installed:
+            libraries = installed.readlines()
+            download_epub(libraries)
     else:
-        print('You did not type anything or did not use the right format.')
+        print('Please enter a list of libraries seperated by a comma')
+        print('If you need to specify a version, please do so by:')
+        print('"name==version"')
+        libraries = str(input(':')).split(',')
+        if libraries:
+            download_epub(libraries)
+        else:
+            print('You did not type anything or did not use the right format.')
 
+if __name__ == '__main__':
+    main(sys.argv[1:])
